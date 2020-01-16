6d65b6b82efd92e1d37f3b35f0c09b8b10eee18e
mgiatti <mb.giatti@gmail.com>
2020-01-16 20:42:53 -0300

    Add cors library

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const databaseUtils = require('./utils/databaseUtils');
+const cors = require('cors');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const app = express();
 
 mongoose.connect(databaseUtils.getDatabaseURI(), databaseUtils.getDatabaseOptions());
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
